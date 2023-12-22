0da94f1d95b44f11bb3a180c16c282e64bdd9694
Jago Stong-Wright <jagoosw@protonmail.com>
2023-12-22 16:54:41 +0000

    added tests for wind stress

---
+++ test/runtests.jl
@@ -0,0 +1,5 @@
+using Walrus, Oceananigans, Documenter, Test
+
+include("test_windstress.jl")
+
+doctest(Walrus)
---
+++ test/test_windstress.jl
@@ -0,0 +1,16 @@
+@testset "Wind stress" begin
+    grid = RectilinearGrid(size = (2, 2, 2), extent = (2, 2, 2))
+
+    wind_stress_boundary_conditions = WindStressBoundaryConditions(; reference_wind_speed = 1., reference_wind_direction = 90.)
+
+    model = NonhydrostaticModel(; grid, boundary_conditions = (u = FieldBoundaryConditions(top = wind_stress_boundary_conditions.u),
+                                                            v = FieldBoundaryConditions(top = wind_stress_boundary_conditions.v)))
+
+    for n=1:100000
+        time_step!(model, 1)
+    end
+
+    @test -1 <= model.velocities.u[1, 1, 2] < 0.1
+    @test model.velocities.u[1, 1, 1] ≈ 0
+    @test model.velocities.v[1, 1, 2] ≈ 0
+end
