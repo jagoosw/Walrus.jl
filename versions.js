260172a27e327c55bf9c58a65ba3b512162e5a7c
Jago Strong-Wright <jagoosw@protonmail.com>
2023-12-28 19:25:43 +0000

    build based on 54c6e0c

--- versions.js
+++ versions.js
@@ -1,5 +1,11 @@
 var DOC_VERSIONS = [
+  "stable",
   "dev",
+  "v0.4.2",
+  "v0.4.1",
+  "v0.4.0",
+  "v0.3.0",
+  "v0.2.0",
 ];
-var DOCUMENTER_NEWEST = "dev";
-var DOCUMENTER_STABLE = "dev";
+var DOCUMENTER_NEWEST = "v0.4.2";
+var DOCUMENTER_STABLE = "stable";
