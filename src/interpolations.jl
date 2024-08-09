a328c21e2ae6dfb53146a3c1a9da95b84f57f73b
Jago Strong-Wright <jagoosw@protonmail.com>
2024-08-09 15:52:44 -0400

    made int possible to be cyclic

--- src/interpolations.jl
+++ src/interpolations.jl
@@ -8,26 +8,35 @@ using Oceananigans.Architectures: on_architecture, CPU
 
 import Adapt: adapt_structure
 
-struct SimpleInterpolation{R, V} <: Function
+struct Linear end
+struct Cyclic end
+
+struct SimpleInterpolation{R, V, M} <: Function
      range :: R
     values :: V
+      mode :: M
 end
 
 adapt_structure(to, itp::SimpleInterpolation) = SimpleInterpolation(adapt(to, itp.range),
                                                                     adapt(to, itp.values))
 
-function SimpleInterpolation(range::Array, values; arch = CPU())
+function SimpleInterpolation(range::Array, values; arch = CPU(), mode = Cyclic())
     x₀ = minimum(range)
+    x₁ = maximum(range)
 
     (range[2] - range[1] ≈ range[end] - range[end - 1]) || throw(ArgumentError("Interpolation range must be regularly spaced"))
 
     Δx = range[2] - range[1]
 
-    return SimpleInterpolation((; x₀, Δx), on_architecture(arch, values))
+    return SimpleInterpolation((; x₀, x₁, Δx), on_architecture(arch, values), mode)
 end
 
+@inline (::Linear)(x, x₀, x₁) = x
+@inline (::Cyclic)(x, x₀, x₁) = mod(x - x₀, x₁ - x₀) + x₀
 
 function (itp::SimpleInterpolation)(x)
+    x = itp.mode(x, itp.range.x₀, itp.range.x₁)
+
     n₁ = floor(Int, (x - itp.range.x₀) / itp.range.Δx)
 
     x₁ = itp.range.x₀ + itp.range.Δx * n₁
--- test/test_interpolation.jl
+++ test/test_interpolation.jl
@@ -7,5 +7,6 @@
     test_x = rand(1000)
 
     # atol is the Rolle theory maxiumum error for linear interpolation
-    @test all([isapprox(itp(x), cos(x), atol = 0.01^2 / 8 * cos(floor(x; digits = 2))) for x in test_x]) 
+    @test all([isapprox(itp(x), cos(x), atol = 0.01^2 / 8 * cos(floor(x; digits = 2))) for x in test_x])
+    @test all([isapprox(itp(x), cos(mod(x, 1)), atol = 0.01^2 / 8 * cos(floor(mod(x, 1); digits = 2))) for x in test_x .+ 1]) 
 end
