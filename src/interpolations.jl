5581dc659b9a8683005a4f449bc74c13e8e62f4f
Jago Strong-Wright <jagoosw@protonmail.com>
2024-08-23 22:39:55 +0100

    some interpolation changes that I guess we need

--- src/interpolations.jl
+++ src/interpolations.jl
@@ -8,30 +8,52 @@ using Oceananigans.Architectures: on_architecture, CPU
 
 import Adapt: adapt_structure
 
-struct Linear end
-struct Cyclic end
-
 struct SimpleInterpolation{R, V, M} <: Function
-     range :: R
-    values :: V
-      mode :: M
+               range :: R
+              values :: V
+  boundary_condition :: M
 end
 
 adapt_structure(to, itp::SimpleInterpolation) = SimpleInterpolation(adapt(to, itp.range),
                                                                     adapt(to, itp.values))
 
-function SimpleInterpolation(range::Array, values; arch = CPU(), mode = Cyclic())
-    x₀ = minimum(range)
-    x₁ = maximum(range)
+function coordinate_to_range(coordinate)
+    x₀ = minimum(coordinate)
+    x₁ = maximum(coordinate)
 
-    (range[2] - range[1] ≈ range[end] - range[end - 1]) || throw(ArgumentError("Interpolation range must be regularly spaced"))
+    (coordinate[2] - coordinate[1] ≈ coordinate[end] - coordinate[end - 1]) || throw(ArgumentError("Interpolation coordinate must be regularly spaced"))
 
-    Δx = range[2] - range[1]
-
-    return SimpleInterpolation((; x₀, x₁, Δx), on_architecture(arch, values), mode)
+    Δx = coordinate[2] - coordinate[1]
+    
+    return (; x₀, x₁, Δx)
 end
 
-@inline function (::Linear)(x, x₀, Δx, N)
+function SimpleInterpolation(coordinate::Array, values; arch = CPU(), boundary_condition = Cyclic())
+    range = coordinate_to_range(coordinate)
+
+    return SimpleInterpolation(range, on_architecture(arch, values), boundary_condition)
+end
+
+function SimpleInterpolation(coordinates::Tuple, values; arch = CPU(), boundary_conditions = Cyclic())
+    if !(boundary_conditions isa Tuple)
+        boundary_conditions = tuple([boundary_conditions for n in 1:length(coordinates)]...)
+    end
+
+    length(coordinates) == length(boundary_conditions) || 
+        throw(ArgumentError("You must either provide one boundary condition to be used in all directions, or one for each direction"))
+
+    ranges = tuple([coordinate_to_range(coord) for coord in coordinates])
+
+    return SimpleInterpolation(ranges, on_architecture(arch, values), boundary_condition)
+end
+
+struct Limited end
+struct Cyclic end
+
+@inline function (::Limited)(x, x₀, Δx, N)
+    # this will (hopeflly) result in an out of bounds error 
+    # but the whole point of this excersise was to make it kernel safe
+    # so I'm not erroring here
     n₀ = floor(Int, (x - x₀) / Δx)
 
     return x, n₀ + 1, n₀ + 2
@@ -49,18 +71,19 @@ end
     return x, n₁, n₂
 end
 
-function (itp::SimpleInterpolation)(x)
-    x₀ = itp.range.x₀
-    x₁ = itp.range.x₁
-    Δx = itp.range.Δx
-    N  = length(itp.values)
+# the terminal case
+function (itp::SimpleInterpolation)(x::Number, values, range, boundary_condition)
+    x₀ = range.x₀
+    x₁ = range.x₁
+    Δx = range.Δx
+    N  = length(values)
 
-    x, n₁, n₂ = itp.mode(x, x₀, Δx, N)
+    x, n₁, n₂ = boundary_condition(x, x₀, Δx, N)
 
     x₁ = x₀ + Δx * (n₁ - 1)
 
-    y₁ = @inbounds itp.values[n₁]
-    y₂ = @inbounds itp.values[n₂]
+    y₁ = @inbounds values[n₁]
+    y₂ = @inbounds values[n₂]
 
     return y₁ + (x - x₁) * (y₂ - y₁) / Δx
 end
