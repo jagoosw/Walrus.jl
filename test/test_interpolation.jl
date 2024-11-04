e392ac62e1a9ed810a02d51566d70c262c55ecb0
Jago Strong-Wright <jagoosw@protonmail.com>
2024-11-04 17:38:28 +0000

    I think this was needed ???

--- test/test_interpolation.jl
+++ test/test_interpolation.jl
@@ -1,14 +1,14 @@
-using Walrus.Interpolations: Linear
+using Walrus.Interpolations: Limited
 
 @testset "Interpolaiton" begin
     x = [0:10^-6:2π;]
     y = cos.(x)
 
-    itp = SimpleInterpolation(x, y; mode = Linear())
+    itp = SimpleInterpolation(x, y; boundary_condition = Limited())
 
     test_x = rand(0:10^-6:2π, 1000)
 
-    # atol is the Rolle theory maxiumum error for linear interpolation + a little
+    # atol is the Rolle theory maxiumum error for Limited interpolation + a little
     @test all([isapprox(itp(x), cos(x), atol = 2 * abs(0.01^2 / 8 * cos(floor(x; digits = 2)))) for x in test_x])
     
     test_x = rand(0:10^-6:4π, 1000)
