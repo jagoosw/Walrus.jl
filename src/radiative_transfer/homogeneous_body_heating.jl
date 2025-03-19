7a441ff560c03977e32077eece3ae949fe0df368
Jago Strong-Wright <jagoosw@protonmail.com>
2025-03-19 16:35:40 +0000

    why didn't this get caught by the doctest fix

--- src/radiative_transfer/homogeneous_body_heating.jl
+++ src/radiative_transfer/homogeneous_body_heating.jl
@@ -61,7 +61,7 @@ julia> model = NonhydrostaticModel(; grid, forcing = (; T = Forcing(body_heating
 NonhydrostaticModel{CPU, RectilinearGrid}(time = 0 seconds, iteration = 0)
 ├── grid: 128×128×128 RectilinearGrid{Float64, Oceananigans.Grids.Periodic, Oceananigans.Grids.Periodic, Oceananigans.Grids.Bounded} on Oceananigans.Architectures.CPU with 3×3×3 halo
 ├── timestepper: RungeKutta3TimeStepper
-├── advection scheme: Centered reconstruction order 2
+├── advection scheme: Centered(order=2)
 ├── tracers: T
 ├── closure: Nothing
 ├── buoyancy: Nothing
