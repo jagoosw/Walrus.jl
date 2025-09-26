918983c46f885e212f8baf9e9c5847361a36ccea
Jago Strong-Wright <jagoosw@protonmail.com>
2025-09-26 10:46:05 +0100

    uncomment some stuff

--- src/Walrus.jl
+++ src/Walrus.jl
@@ -24,16 +24,12 @@ include("wall_model.jl")
 include("radiative_transfer/radiative_transfer.jl")
 include("tidal_forcings.jl")
 include("SurfaceFluxModel/SurfaceFluxModel.jl")
-#include("wind_stress.jl")
-#include("wind_driven_stokes.jl")
-#include("surface_heating.jl")
+include("wind_driven_stokes.jl")
 
 using .Interpolations
 using .WallStressModel
 using .RadiativeTransfer
 using .TidalForcing
-#using .WindStressModel
-#using .SurfaceHeatingModel
-#using .WindDrivenStokesParameterisation
+using .WindDrivenStokesParameterisation
 
 end # module Walrus
